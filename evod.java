d71da193ebb6829cd960927ec4298b4e8687abeb
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-15 14:19:58 -0400

    Create evod.java

---
+++ evod.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class evod
+{
+public static void main(String args[])
+{
+int num;
+Scanner a=new Scanner(System.in);
+num=a.nextInt();
+if(num%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
